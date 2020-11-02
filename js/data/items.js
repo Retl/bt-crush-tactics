e00de15b55614c457ba864d8068153eac37772d2
Carlis Moore <carlis.moore@gmail.com>
2020-11-02 00:29:08 -0600

    Update items.js

--- js/data/items.js
+++ js/data/items.js
@@ -5,4 +5,43 @@ if (typeof BTCT == 'undefined' || BTCT == null)
 
 BTCT["ITEMS"] = [];
 
+// Canon: FP
 
+
+// Canon: GFR
+
+
+// Canon: PC
+
+
+// Canon: CTH
+
+
+// Canon: SB
+
+
+// Canon: HF
+
+
+// Canon: TREE
+
+
+// Canon: XGR
+
+
+// Canon: BK
+
+
+// Canon: TUPR
+
+
+// Canon: SC
+
+
+// Canon: NTS
+
+
+// Canon: RUF
+
+
+// Canon: WILD
